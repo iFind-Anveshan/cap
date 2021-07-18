e79fa8bfd4b2c2a412da205680fe8c3e102dc40c
ydshieh <ydshieh@user.noreply>
2021-07-18 10:24:37 +0200

    fix model path

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ sys.path.append(current_path)
 # Main model -  ViTGPT2LM
 from vit_gpt2.modeling_flax_vit_gpt2_lm import FlaxViTGPT2LMForConditionalGeneration
 
-model_name_or_path = 'flax-community/vit-gpt2/checkpoints/ckpt_5/'
+model_name_or_path = 'flax-community/vit-gpt2/checkpoints/ckpt_5'
 flax_vit_gpt2_lm = FlaxViTGPT2LMForConditionalGeneration.from_pretrained(model_name_or_path)
 
 def predict(image):
