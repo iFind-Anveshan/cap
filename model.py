13b9f3b8c862776ab79f62ccb031b600a8466d47
ydshieh <ydshieh@user.noreply>
2021-07-18 17:58:27 +0200

    fix caption

--- model.py
+++ model.py
@@ -53,7 +53,7 @@ def predict(image):
     token_ids = np.array(generation.sequences)[0]
     caption = tokenizer.decode(token_ids)
     caption = caption.replace('<s>', '').replace('</s>', '').replace('<pad>', '')
-    caption = caption.replace("à l'arrière-plan", '').replace("Une photo noire et blanche d'", '').replace("en arrière-plan", '')
+    caption = caption.replace("à l'arrière-plan", '').replace("Une photo en noir et blanc d", '').replace("Une photo noire et blanche d'", '').replace("en arrière-plan", '')
     while '  ' in caption:
         caption = caption.replace('  ', ' ')
     caption = caption.strip()
