e75500983337a80913cc195c4df9f5c927c278f0
ydshieh <ydshieh@user.noreply>
2021-07-18 10:15:05 +0200

    try load model from hub

--- model.py
+++ model.py
@@ -9,11 +9,13 @@ from transformers import GPT2Tokenizer
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_path)
 
-# Main model -  ViTGPT2LM
-# from vit_gpt2.modeling_flax_vit_gpt2_lm import FlaxViTGPT2LMForConditionalGeneration
+Main model -  ViTGPT2LM
+from vit_gpt2.modeling_flax_vit_gpt2_lm import FlaxViTGPT2LMForConditionalGeneration
+
+model_name_or_path = 'flax-community/vit-gpt2/checkpoints/ckpt_5/'
+flax_vit_gpt2_lm = FlaxViTGPT2LMForConditionalGeneration.from_pretrained(model_name_or_path)
 
 def predict(image):
-
     return 'dummy caption!', ['dummy', 'caption', '!'], [1, 2, 3]
 
 
